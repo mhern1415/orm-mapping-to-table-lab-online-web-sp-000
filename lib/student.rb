ab46b5993d81128d430e2ba9005f280beeeef420
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 22:32:44 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -2,7 +2,7 @@ class Student
 attr_accessor :name, :grade
 attr_reader :id 
 
-def initialize (name, grade, id)
+def initialize (name, grade, id=nil)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
