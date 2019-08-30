0432c3e95d27982f1a4825dd377a10b252eb2ef3
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 22:41:46 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,7 +28,7 @@ end
 
 def save
   sql = <<-SQL
-  INSERT 
+  INSERT INTO students (name, grade)
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
