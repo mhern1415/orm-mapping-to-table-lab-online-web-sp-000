0db8c1c59f960ca1ac3e423241e861357f08c581
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 22:34:16 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -7,6 +7,9 @@ def initialize (name, grade, id=nil)
   @name = name
   @grade = grade
 end
+
+def create_table
+  
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
   
