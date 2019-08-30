0382109dcb945a79e98cecd6a2fdba9746f4a35c
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 22:36:59 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -13,7 +13,7 @@ def create_table
       CREATE TABLE IF NOT EXISTS students (
         id INTEGER PRIMARY KEY, 
         name TEXT, 
-        grade TEXT
+        grade INTEGER
         )
         SQL
     DB[:conn].execute(sql) 
