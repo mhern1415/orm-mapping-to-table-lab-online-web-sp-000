d59987cfc72232cdd2e9239558b3fa6b55fac47e
Mark Hernandez <m.hern1415@gmail.com>
2019-08-30 22:43:56 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -28,11 +28,12 @@ end
 
 def save
   sql = <<-SQL
-  INSERT INTO students (name, grade) VALUES (?, ?)
-  SQL 
-  
-  DB[:conn].execute(sql, self.name, self.grade)
-end
+  INSERT INTO students (name, grade) 
+  VALUES (?, ?)
+  SQL
+ 
+    DB[:conn].execute(sql, self.name, self.album)
+  end
 
   # Remember, you can access your database connection anywhere in this class
   #  with DB[:conn]  
